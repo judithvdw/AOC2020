ef300d4a1a6de9cb4fee4b2dead33c5ededf31e1
Judith <judith.vdwetering@gmail.com>
2020-12-05 10:10:56 +0100

    indentation

--- day_04/d4.py
+++ day_04/d4.py
@@ -85,8 +85,8 @@ if __name__ == '__main__':
     with open("4.txt") as f:
         blob = f.read()
 
-passports = parse_input(blob)
+    passports = parse_input(blob)
 
-part_1, part_2 = check_passports(passports)
-print("Part 1: ", part_1)
-print("Part 2: ", part_2)
+    part_1, part_2 = check_passports(passports)
+    print("Part 1: ", part_1)
+    print("Part 2: ", part_2)
