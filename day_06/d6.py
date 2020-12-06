3be82eec2d2bebba3fefdee3949ea1ee2c433cd9
Judith <judith.vdwetering@gmail.com>
2020-12-06 08:37:10 +0100

    day 6

--- day_06/d6.py
+++ day_06/d6.py
@@ -1,27 +1,29 @@
-def somebody_yes(forms):
-    unique_answers = set()
-    for form in forms:
-        unique_answers.update(list(form))
-    return len(unique_answers)
+def somebody_yes(form_groups):
+    totals = []
+    for form_group in form_groups:
+        unique_answers = set()
+        for form in form_group:
+            unique_answers.update(list(form))
+        totals.append(len(unique_answers))
+    return totals
 
-def all_yes(forms):
-    letter_forms = [set(list(form)) for form in forms]
-    yes = set.intersection(*letter_forms)
-    return len(yes)
+
+def all_yes(form_groups):
+    totals = []
+    for form_group in form_groups:
+        letter_forms = [set(list(form)) for form in form_group]
+        yes = set.intersection(*letter_forms)
+        totals.append(len(yes))
+    return totals
 
 
 def parse_input(blob: str):
-    form_groups = [passport.strip().split() for passport in blob.split("\n\n")]
-    totals = []
-    print(form_groups)
-    for form_group in form_groups:
-        totals.append(all_yes(form_group))
-    return totals
+    return [passport.strip().split() for passport in blob.split("\n\n")]
 
 
 if __name__ == '__main__':
     with open('6.txt') as f:
-        blob = f.read()
+        forms = parse_input(f.read())
 
-    #print("part 1", sum(parse_input(blob)))
-    print("part 2", sum(parse_input(blob)))
+    print("part 1", sum(somebody_yes(forms)))
+    print("part 2", sum(all_yes(forms)))
